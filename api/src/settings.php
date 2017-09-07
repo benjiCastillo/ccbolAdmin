e7b781734d49af7b1cc03378bfa5060ce34a6e93
Erwin Méndez <erwinXYZ1@gmail.com>
2017-09-07 08:49:20 -0400

    ccbol_db

--- api/src/settings.php
+++ api/src/settings.php
@@ -19,10 +19,10 @@ return [
         //configuracion del Token y la base de datos
         'app_token_name'    => 'x#Je3r',
         'connectionString'  =>[
-            'dns'   => 'mysql:host=localhost;dbname=ccbol;charset=utf8',
+            'dns'   => 'mysql:host=localhost;dbname=ccbol_db;charset=utf8',
             'user'  =>  'root',
             'pass'  =>  '',
-            'name_db' => 'ccbol',
+            'name_db' => 'ccbol_db',
             'host' => 'localhost'
         ]
 
